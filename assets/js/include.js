fa22988db6796bf41fd77630c3b942cd5fb3d5d9
fatimatwii <fatimaatwi886@gmail.com>
2024-05-26 03:20:29 +0300

    doctor dashboard

--- assets/js/include.js
+++ assets/js/include.js
@@ -8,27 +8,6 @@ menuToggle.onclick = function () {
   menuToggle.classList.toggle('h-active');
   sidebar.classList.toggle('h-active');
 
-  // Adjust main margin-left based on window width
-  if (menuToggle.classList.contains('h-active')) {
-    main.style.marginLeft = "";
-    main.style.transition = "margin 0.5s ease";
-  } else {
-    if (window.innerWidth <= 500) {
-      main.style.marginLeft = "0";
-    } else {
-      main.style.marginLeft = "15%";
-    }
-    main.style.transition = "margin 0.5s ease";
-  }
-}
-
-window.onresize = function () {
-  // Adjust main margin-left on window resize
-  if (!menuToggle.classList.contains('h-active') && window.innerWidth <= 500) {
-    main.style.marginLeft = "0";
-  } else if (!menuToggle.classList.contains('h-active')) {
-    main.style.marginLeft = "15%";
-  }
 }
 
 let listItems = document.querySelectorAll('.h-navigation ul li');
@@ -54,5 +33,3 @@ $(document).ready(function () {
   });
 });
 
-// Trigger click event on menuToggle to expand navigation bar by default
-menuToggle.click();
